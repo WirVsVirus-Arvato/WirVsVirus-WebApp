3184316875221990c59e5734847c5f5e36d93313
Ferhat Kalinci <ferhat.kalinci@arvato.com>
2020-03-21 20:08:39 +0100

    Removed duplicate imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,6 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import localeEn from '@angular/common/locales/en';
-import localeDe from '@angular/common/locales/de';
 import { registerLocaleData } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
