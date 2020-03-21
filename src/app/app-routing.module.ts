686016a306187c871f207fa9f7f1d2ebc2d7f569
Ferhat Kalinci <ferhat.kalinci@arvato.com>
2020-03-21 13:53:36 +0100

    Added pages to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { OnboardingComponent } from './onboarding/onboarding.component';
+import { OnboardingFinishedComponent } from './onboarding-finished/onboarding-finished.component';
+import { StatusGoodComponent } from './status-good/status-good.component';
+import { StatusNotGoodComponent } from './status-not-good/status-not-good.component';
+import { StatusTrackerComponent } from './status-tracker/status-tracker.component';
 
 
 const appRoutes: Routes = [
-
+  { path: 'welcome', component: LandingPageComponent },
+  { path: 'onboarding', component: OnboardingComponent },
+  { path: 'onboarding-finished', component: OnboardingFinishedComponent },
+  { path: 'statustracker', component: StatusTrackerComponent },
+  { path: 'status-good', component: StatusGoodComponent },
+  { path: 'status-not-good', component: StatusNotGoodComponent },
+  { path: '**', component: LandingPageComponent }
 ];
 
 @NgModule({
