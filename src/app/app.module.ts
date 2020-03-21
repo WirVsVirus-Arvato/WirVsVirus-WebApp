5bf75610aa2b468bb326822d4468ddef6eaad439
scre127 <julian.schrewe@bertelsmann.de>
2020-03-21 14:09:21 +0100

    add content to landing-page.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,28 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OnboardingComponent } from './onboarding/onboarding.component';
 import { OnboardingFinishedComponent } from './onboarding-finished/onboarding-finished.component';
+import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {StatusTrackerComponent} from "./status-tracker/status-tracker.component";
 import { StatusGoodComponent } from './status-good/status-good.component';
 import { StatusNotGoodComponent } from './status-not-good/status-not-good.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     OnboardingComponent,
     OnboardingFinishedComponent,
+    LandingPageComponent,
     StatusTrackerComponent,
     StatusGoodComponent,
     StatusNotGoodComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
