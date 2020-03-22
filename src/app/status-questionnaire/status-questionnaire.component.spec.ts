aa2c213c6851eadb981ff2263b85238d96d373f7
Ferhat Kalinci <ferhat.kalinci@arvato.com>
2020-03-22 16:33:14 +0100

    Fix tests

--- src/app/status-questionnaire/status-questionnaire.component.spec.ts
+++ src/app/status-questionnaire/status-questionnaire.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StatusQuestionnaireComponent } from './status-questionnaire.component';
 import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('StatusQuestionnaireComponent', () => {
   let component: StatusQuestionnaireComponent;
@@ -11,7 +12,8 @@ describe('StatusQuestionnaireComponent', () => {
     TestBed.configureTestingModule({
       declarations: [StatusQuestionnaireComponent ],
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ]
     })
     .compileComponents();
