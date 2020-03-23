7a475c8c22dc145c06b10d494380d9ad85facbb3
Theresa Lampe <theresa.lampe@googlemail.com>
2020-03-23 20:12:00 +0100

    Add environment.json file for update apiUrl on runtime

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OnboardingComponent } from './onboarding/onboarding.component';
-import { OnboardingFinishedComponent } from './onboarding-finished/onboarding-finished.component';
-import { StatusTrackerComponent } from './status-tracker/status-tracker.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { StatusQuestionnaireComponent } from './status-questionnaire/status-questionnaire.component';
-import { AppRoutingModule } from './app-routing.module';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { registerLocaleData } from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OnboardingComponent} from './onboarding/onboarding.component';
+import {OnboardingFinishedComponent} from './onboarding-finished/onboarding-finished.component';
+import {StatusTrackerComponent} from './status-tracker/status-tracker.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
+import {StatusQuestionnaireComponent} from './status-questionnaire/status-questionnaire.component';
+import {AppRoutingModule} from './app-routing.module';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {registerLocaleData} from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import localeEn from '@angular/common/locales/en';
 import localeDe from '@angular/common/locales/de';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import { QuestionnaireComponent } from './shared/questionnaire/questionnaire.component';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSelectModule } from '@angular/material/select';
-import { HttpClientModule } from '@angular/common/http';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatListModule } from '@angular/material/list';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {QuestionnaireComponent} from './shared/questionnaire/questionnaire.component';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
+import {HttpClientModule} from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {environment} from '../environments/environment';
+import {from} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 registerLocaleData(localeEn, 'en-GB');
 registerLocaleData(localeDe, 'de-DE');
+
+const initApp = () => {
+  return () => {
+    return from(
+      fetch('assets/config/environment.json').then(function(response) {
+        return response.json();
+      })
+    ).pipe(
+      map((config) => {
+        console.log(config);
+        environment.production = config.production;
+        environment.apiUrl = config.apiUrl;
+      })).toPromise();
+  };
+};
 
 @NgModule({
   declarations: [
@@ -67,7 +84,13 @@ registerLocaleData(localeDe, 'de-DE');
     MatListModule,
     MatSnackBarModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initApp,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
